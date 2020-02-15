52344a91e2a49af34c4ea7af72c1c8262479e237
lcrodrigues <leonardocr95@gmail.com>
2020-02-15 14:44:27 -0300

    Criação do arquivo Utils.

--- app/src/main/java/com/example/heroeslist/ui/heroes/HeroesFragment.kt
+++ app/src/main/java/com/example/heroeslist/ui/heroes/HeroesFragment.kt
@@ -1,6 +1,5 @@
 package com.example.heroeslist.ui.heroes
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
---
+++ app/src/main/java/com/example/heroeslist/util/Utils.kt
@@ -0,0 +1,4 @@
+package com.example.heroeslist.util
+
+const val API_KEY = "8f4214a98e94658ffa0449436e102b43"
+const val BASE_URL = "http://gateway.marvel.com/docs/public/"
