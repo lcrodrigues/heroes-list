9b1a49806f75637c1463750808ab23f1427af422
lcrodrigues <leonardocr95@gmail.com>
2020-02-16 14:48:20 -0300

    Troca actionbar por toolbar.

--- app/src/main/java/com/example/heroeslist/ui/heroes/HeroesActivity.kt
+++ app/src/main/java/com/example/heroeslist/ui/heroes/HeroesActivity.kt
@@ -3,11 +3,16 @@ package com.example.heroeslist.ui.heroes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.heroeslist.R
+import kotlinx.android.synthetic.main.activity_heroes.*
 
 class HeroesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_heroes)
+
+        appToolbar.title = resources.getString(R.string.app_name)
+        setSupportActionBar(appToolbar)
+
     }
 }
