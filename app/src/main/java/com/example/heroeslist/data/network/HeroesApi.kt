6d8b99775756403cb0bb75ee6a85e66a79d8ea5a
lcrodrigues <leonardocr95@gmail.com>
2020-02-15 21:15:33 -0300

    Interface para chamada de GET com Retrofit.

--- app/src/main/java/com/example/heroeslist/data/network/HeroesApi.kt
+++ app/src/main/java/com/example/heroeslist/data/network/HeroesApi.kt
@@ -1,5 +1,32 @@
 package com.example.heroeslist.data.network
 
+
+import retrofit2.Call
+import com.example.heroeslist.data.response.CharacterWrapperResponse
+import com.example.heroeslist.util.PUBLIC_KEY
+import com.example.heroeslist.util.BASE_URL
+import com.example.heroeslist.util.getMD5
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface HeroesApi {
 
+    @GET("characters")
+    suspend fun getHeroes(
+        @Query("apikey") apiKey: String = PUBLIC_KEY,
+        @Query("ts") ts: String,
+        @Query("hash") hash: String = getMD5(ts)
+    ): Call<CharacterWrapperResponse>
+
+    companion object {
+        operator fun invoke(): HeroesApi {
+            return Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+                .create(HeroesApi::class.java)
+        }
+    }
 }
