3e5d5ecfa4c31a09cbf4efc39c932044cf1478bb
Leonardo Correa Rodrigues <lrodrigues@nelogica.com.br>
2020-02-17 10:53:17 -0300

    Retira logs.

--- app/src/main/java/com/example/heroeslist/util/BindingUtils.kt
+++ app/src/main/java/com/example/heroeslist/util/BindingUtils.kt
@@ -1,13 +1,11 @@
 package com.example.heroeslist.util
 
-import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("image")
 fun loadImage(view: ImageView, url: String?) {
     if (url != null) {
-        Log.d("17022020", url)
         GlideApp.with(view.context).load(url).into(view)
     }
 }
